a0175a3e4a560a31f067f295f367f64bd8030763
Summit <haquesu@oregonstate.edu>
2025-10-22 18:07:53 -0700

    xy - updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -25,7 +25,9 @@ public class DeveloperTest {
         assertEquals("Summit H.", Developer.getName());
     }
 
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void getGithubId_returns_correct_githubId() {
+        assertEquals("Summit-Haque", Developer.getGithubId());
+    }
 
 }
